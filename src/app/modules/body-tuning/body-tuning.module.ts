07c8397fbfaeb15e5603bcb3395a1c02067c0dc3
lachetti <64404986+lachetti@users.noreply.github.com>
2023-02-15 15:31:46 +0300

    output test (#2)
    * prod build script
    
    * output test

--- src/app/modules/body-tuning/body-tuning.module.ts
+++ src/app/modules/body-tuning/body-tuning.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExerciseCardComponent } from './components/exercise-card/exercise-card.component';
+import { BodyTuningComponent } from './body-tuning.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: BodyTuningComponent },
+];
 
 @NgModule({
   declarations: [
-    ExerciseCardComponent
+    ExerciseCardComponent,
+    BodyTuningComponent,
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes),
+  ],
 })
-export class BodyTuningModule { }
+export class BodyTuningModule {}
