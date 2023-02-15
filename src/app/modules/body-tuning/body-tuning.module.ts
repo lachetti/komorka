83c2291da1635e9c4f2ff68f4b439d7489725e47
lachetti <iluja.mineev@gmail.com>
2023-02-15 13:34:42 +0300

    init angular

---
+++ src/app/modules/body-tuning/body-tuning.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExerciseCardComponent } from './components/exercise-card/exercise-card.component';
+
+@NgModule({
+  declarations: [
+    ExerciseCardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BodyTuningModule { }
